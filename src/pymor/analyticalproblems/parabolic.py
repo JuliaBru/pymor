968b8b7c5a38d983038bb6856162da1d7451670b
Julia Brunken <julia.brunken@uni-muenster.de>
2016-07-29 14:56:36 +0200

    Parabolic also containing lower order terms like new version of elliptic

--- src/pymor/analyticalproblems/parabolic.py
+++ src/pymor/analyticalproblems/parabolic.py
@@ -7,6 +7,7 @@ from pymor.core.interfaces import ImmutableInterface
 from pymor.analyticalproblems.elliptic import EllipticProblem
 from pymor.domaindescriptions.basic import RectDomain
 from pymor.functions.basic import ConstantFunction
+import numpy as np
 
 
 class ParabolicProblem(ImmutableInterface):
@@ -14,10 +15,10 @@ class ParabolicProblem(ImmutableInterface):
 
     The problem consists in solving ::
 
-    |                       K
-    |  ∂_t u(x, t, μ) - ∇ ⋅ ∑  θ_k(μ) ⋅ d_k(x) ∇ u(x, t, μ) = f(x, t, μ)
-    |                      k=0
-    |                                            u(x, 0, μ) = u_0(x, μ)
+    |                      Kd                                   Kv                            Kr
+    | ∂_t u(x, t, μ) - ∇ ⋅ ∑ θ_k(μ) ⋅ d_k(x) ∇ u(x, t, μ) + ∇ ⋅ ∑ θ_{v,k}(μ) v_k(x) u(x, μ) + ∑ θ_{r,k}(μ) r_k(x) u(x, μ) = f(x, t, μ)
+    |                     k=0                                  k=0                           k=0
+    |                                                                                                          u(x, 0, μ) = u_0(x, μ)
 
     for u with t in [0, T], x in Ω.
 
@@ -35,10 +36,26 @@ class ParabolicProblem(ImmutableInterface):
         List containing the |ParameterFunctionals| θ_k(μ). If
         `len(diffusion_functions) == 1`, `diffusion_functionals` is allowed
         to be `None`, in which case no parameter dependence is assumed.
+    advection_functions
+        List containing the |Functions| v_k(x), each having `shape_range`
+        of `(dim domain, )`.
+    advection_functionals
+        List containing the |ParameterFunctionals| θ_{v,k}(μ). If
+        `len(advection_functions) == 1`, `advection_functionals` is allowed
+        to be `None`, in which case no parameter dependence is assumed.
+    reaction_functions
+        List containing the |Functions| r_k(x), each having `shape_range`
+        of `tuple()`.
+    reaction_functionals
+        List containing the |ParameterFunctionals| θ_{r,k}(μ). If
+        `len(reaction_functions) == 1`, `reaction_functionals` is allowed
+        to be `None`, in which case no parameter dependence is assumed.
     dirichlet_data
         |Function| providing the Dirichlet boundary values in global coordinates.
     neumann_data
         |Function| providing the Neumann boundary values in global coordinates.
+    robin_data
+        Tuple of two |Functions| providing the Robin parameter and boundary values.
     parameter_space
         |ParameterSpace| for the problem.
     initial_data
@@ -54,29 +71,79 @@ class ParabolicProblem(ImmutableInterface):
     rhs
     diffusion_functions
     diffusion_functionals
+    advection_functions
+    advection_functionals
+    reaction_functions
+    reaction_functionals
     dirichlet_data
     neumann_data
+    robin_data
     initial_data
     T
     """
 
     def __init__(self, domain=RectDomain(), rhs=ConstantFunction(dim_domain=2),
-                 diffusion_functions=(ConstantFunction(dim_domain=2),), diffusion_functionals=None, dirichlet_data=None,
-                 neumann_data=None, initial_data=ConstantFunction(dim_domain=2), T=1, parameter_space=None, name=None):
+                 diffusion_functions=None,
+                 diffusion_functionals=None,
+                 advection_functions=None,
+                 advection_functionals=None,
+                 reaction_functions=None,
+                 reaction_functionals=None,
+                 dirichlet_data=None, neumann_data=None, robin_data=None,
+                 initial_data=ConstantFunction(dim_domain=2), T=1,
+                 parameter_space=None, name=None):
 
-        assert isinstance(diffusion_functions, (tuple, list))
-        assert diffusion_functionals is None and len(diffusion_functions) == 1 \
+        assert diffusion_functions is None or isinstance(diffusion_functions, (tuple, list))
+        assert advection_functions is None or isinstance(advection_functions, (tuple, list))
+        assert reaction_functions is None or isinstance(reaction_functions, (tuple, list))
+
+        assert diffusion_functionals is None and diffusion_functions is None \
+            or diffusion_functionals is None and len(diffusion_functions) == 1 \
             or len(diffusion_functionals) == len(diffusion_functions)
-        assert rhs.dim_domain == diffusion_functions[0].dim_domain
-        assert dirichlet_data is None or dirichlet_data.dim_domain == diffusion_functions[0].dim_domain
-        assert neumann_data is None or neumann_data.dim_domain == diffusion_functions[0].dim_domain
-        assert initial_data is None or initial_data.dim_domain == diffusion_functions[0].dim_domain
+        assert advection_functionals is None and advection_functions is None \
+            or advection_functionals is None and len(advection_functions) == 1 \
+            or len(advection_functionals) == len(advection_functions)
+        assert reaction_functionals is None and reaction_functions is None \
+            or reaction_functionals is None and len(reaction_functions) == 1 \
+            or len(reaction_functionals) == len(reaction_functions)
+
+        # for backward compatibility:
+        if (diffusion_functions is None and advection_functions is None and reaction_functions is None):
+            diffusion_functions = (ConstantFunction(dim_domain=2),)
+
+
+        # dim_domain:
+        if diffusion_functions is not None:
+            dim_domain = diffusion_functions[0].dim_domain
+
+        assert rhs.dim_domain == dim_domain
+        if diffusion_functions is not None:
+            for f in diffusion_functions:
+                assert f.dim_domain == dim_domain
+        if advection_functions is not None:
+            for f in advection_functions:
+                assert f.dim_domain == dim_domain
+        if reaction_functions is not None:
+            for f in reaction_functions:
+                assert f.dim_domain == dim_domain
+
+
+        assert dirichlet_data is None or dirichlet_data.dim_domain == dim_domain
+        assert neumann_data is None or neumann_data.dim_domain == dim_domain
+        assert robin_data is None or (isinstance(robin_data, tuple) and len(robin_data) == 2)
+        assert robin_data is None or np.all([f.dim_domain == dim_domain for f in robin_data])
+        assert initial_data is None or initial_data.dim_domain == dim_domain
         self.domain = domain
         self.rhs = rhs
         self.diffusion_functions = diffusion_functions
         self.diffusion_functionals = diffusion_functionals
+        self.advection_functions = advection_functions
+        self.advection_functionals = advection_functionals
+        self.reaction_functions = reaction_functions
+        self.reaction_functionals = reaction_functionals
         self.dirichlet_data = dirichlet_data
         self.neumann_data = neumann_data
+        self.robin_data = robin_data
         self.initial_data = initial_data
         self.T = T
         self.parameter_space = parameter_space
@@ -86,9 +153,16 @@ class ParabolicProblem(ImmutableInterface):
     def from_elliptic(cls, elliptic_problem, initial_data=ConstantFunction(dim_domain=2), T=1):
         assert isinstance(elliptic_problem, EllipticProblem)
 
-        return cls(elliptic_problem.domain, elliptic_problem.rhs, elliptic_problem.diffusion_functions,
-                   elliptic_problem.diffusion_functionals, elliptic_problem.dirichlet_data,
-                   elliptic_problem.neumann_data, initial_data, T, elliptic_problem.parameter_space,
+        return cls(elliptic_problem.domain, elliptic_problem.rhs,
+                   elliptic_problem.diffusion_functions,
+                   elliptic_problem.diffusion_functionals,
+                   elliptic_problem.advection_functions,
+                   elliptic_problem.advection_functionals,
+                   elliptic_problem.reaction_functions,
+                   elliptic_problem.reaction_functionals,
+                   elliptic_problem.dirichlet_data, elliptic_problem.neumann_data,
+                   elliptic_problem.robin_data,
+                   initial_data, T, elliptic_problem.parameter_space,
                    'ParabolicProblem_from_{}'.format(elliptic_problem.name))
 
     def elliptic_part(self):
@@ -97,6 +171,10 @@ class ParabolicProblem(ImmutableInterface):
             rhs=self.rhs,
             diffusion_functions=self.diffusion_functions,
             diffusion_functionals=self.diffusion_functionals,
+            advection_functions=self.advection_functions,
+            advection_functionals=self.advection_functionals,
+            reaction_functions=self.reaction_functions,
+            reaction_functionals=self.reaction_functionals,
             dirichlet_data=self.dirichlet_data,
             neumann_data=self.neumann_data,
             parameter_space=self.parameter_space,
